fc5aa2587cbef9a97dccb1b5851b9ef8676c92c9
joysondeveloper-cell <joysondeveloper@gmail.com>
2026-06-28 14:10:05 +0530

    changes

--- backend/index.js
+++ backend/index.js
@@ -7,10 +7,15 @@ const app = express();
 app.use(cors());
 
 // mongoose.connect("mongodb://127.0.0.1:27017/admin");
-mongoose.connect(
+// mongoose.connect(
   // "mongodb+srv://joyson130497_db_user:HzzWVtxXQioV8vZX@cluster0.smf82qb.mongodb.net/user?retryWrites=true&w=majority&appName=Cluster0"
-  "mongodb+srv://joyson041997_db_user:HzzWVtxXQioV8vZX@cluster0.smf82qb.mongodb.net/user?appName=Cluster0"
-);
+//   "mongodb+srv://joyson041997_db_user:HzzWVtxXQioV8vZX@cluster0.smf82qb.mongodb.net/user?appName=Cluster0"
+// );
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB Connected"))
+  .catch((err) => console.error("MongoDB Error:", err));
 
 const EmployeeSchema = new mongoose.Schema({
   name: String,
@@ -33,7 +38,7 @@ app.get("/employees", async (req, res) => {
 
 
 // const PORT = process.env.PORT || 3000;
-const PORT = process.env.MONGO_URI || 3000;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
